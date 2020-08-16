361cf8d999ad781f34909515f532bb6d08678785
Roxana Ailoaie <r.ailoaie@stabiplan.ro>
2020-08-16 23:58:10 +0300

    added toggle functions

--- menu.js
+++ menu.js
@@ -1,14 +1,18 @@
 
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
-
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
 
-let dropdown = document.getElementById('js-dropdown');
-let dropdownToggle = document.getElementById('js-dropdown-toggle');
-
-dropdownToggle.addEventListener('click', function () {
+let dropdown = document.getElementById('js-dropdown_men');
+let dropdownMenToggle = document.getElementById('js-dropdown-toggle_men');
+dropdownMenToggle.addEventListener('click', function () {
   dropdown.classList.toggle('active');
+});
+
+let dropdownWomen = document.getElementById('js-dropdown_women');
+let dropdownWomenToggle = document.getElementById('js-dropdown-toggle_wowmen');
+dropdownWomenToggle.addEventListener('click', function () {
+  dropdownWomen.classList.toggle('active');
 });
