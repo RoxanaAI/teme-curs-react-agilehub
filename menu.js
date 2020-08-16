7aabfcc2e74cc37a4d9d24fa3a134441f6d16a6b
Roxana Ailoaie <r.ailoaie@stabiplan.ro>
2020-08-16 23:37:10 +0300

    add event listener

--- menu.js
+++ menu.js
@@ -5,3 +5,10 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+let dropdown = document.getElementById('js-dropdown');
+let dropdownToggle = document.getElementById('js-dropdown-toggle');
+
+dropdownToggle.addEventListener('click', function () {
+  dropdown.classList.toggle('not-active');
+});
